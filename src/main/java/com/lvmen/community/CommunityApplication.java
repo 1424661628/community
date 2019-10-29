55f2acc1949fb30e7644c0ac7b9e37b5d8ed422a
LMH <1424661628@qq.com>
2019-10-29 17:19:18 +0800

    init repo

---
+++ src/main/java/com/lvmen/community/CommunityApplication.java
@@ -0,0 +1,13 @@
+package com.lvmen.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
